2d681600c8e73c5ae1110052ed10542e2848564e
calcudaus <dausg878@gmail.com>
2024-11-02 12:20:54 +0700

    USER UPDATE

--- segmen/semua/form_add_user.php
+++ segmen/semua/form_add_user.php
@@ -1,3 +1,15 @@
+<div class="row">
+	<div class="col-12 p-0">
+		<div class="breadcrumbs">
+			<ul class="breadcrumb-nav ps-3">
+				<li><a href="">Dashboard</a></li>
+				<li><a href="">Pengguna</a></li>
+				<li class="text-primary">Tambah Data Pengguna</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- End Breadcrumbs -->
 <div class="box">
 	<h3 class="title is-5">Tambah Data Pengguna</h3>
 	<hr>
@@ -29,128 +41,145 @@
 		echo $notif;
 	}
 	?>
-	<form action="" method="post" enctype="multipart/form-data">
-		<div class="container">
-			<div class="row">
-				<div class="col-6">
-					<div class="field">
-						<label>Nama Lengkap:</label>
-						<div class="control">
-							<input name="nama" type="text" class="form-control">
-						</div>
-					</div>
-
-					<div class="field">
-						<label>Email:</label>
-						<div class="control">
-							<input name="email" type="email" class="form-control">
-						</div>
-					</div>
-
-					<div class="field">
-						<label>Password:</label>
-						<div class="control">
-							<input name="password" type="password" class="form-control">
-						</div>
-					</div>
-
-					<div class="columns">
-						<div class="column is-half">
-							<div class="field">
-								<label>Jenis Kelamin:</label>
-								<div class="control">
-									<div class="select is-fullwidth">
-										<select name="kelamin" class="form-select">
-											<option value="pria"> Pria </option>
-											<option value="wanita"> Wanita </option>
-										</select>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-
-				</div>
-				<div class="col-6">
-					<div class="column is-half">
+	<div class="container card border-0 shadow rounded-4 p-3">
+		<form action="" method="post" enctype="multipart/form-data">
+			<div class="container">
+				<div class="row">
+					<div class="col">
 						<div class="field">
-							<label>Hak Akses:</label>
+							<label class="mt-2">Nama Lengkap:</label>
 							<div class="control">
-								<div class="select is-fullwidth">
-									<select name="level" class="form-select">
-										<option value="operator"> Operator </option>
-										<option value="viewer"> Viewer </option>
-										<option value="admin"> Admin </option>
-									</select>
-								</div>
+								<input name="nama" type="text" class="form-control">
 							</div>
 						</div>
-					</div>
-
-					<div class="columns">
-						<div class="column is-half">
-							<div class="field">
-								<label>Bagian Kontrol:</label>
-								<div class="control">
-									<div class="select is-fullwidth">
-										<select name="segmen" class="form-select">
-											<option value="imb"> IMB </option>
-											<option value="krk"> KRK </option>
-											<option value="pengawasan"> Pengawasan </option>
-											<option value="semua"> Semua Bagian </option>
-										</select>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="column is-half">
-							<div class="field">
-								<label>Status:</label>
-								<div class="control">
-									<div class="select is-fullwidth">
-										<select name="status" class="form-select">
-											<option value="aktif"> Aktif </option>
-											<option value="tidak aktif"> Tidak Aktif </option>
-										</select>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div class="field">
-						<label>Foto</label>
-						<div class="control">
-							<div class="file">
-								<label class="file-label d-flex align-items-center">
-									<input class="form-control" type="file" name="resume" />
-									<span class="file-cta ps-2" style="width: 250px;">
-										<span class="file-icon">
-											<i class="fas fa-upload"></i>
-										</span>
-										<span class="file-label"> Pilih file foto… </span>
-									</span>
-								</label>
-							</div>
-						</div>
-					</div>
-
-					<div class="field has-text-right mt-3 text-end">
-						<button name="tambah" class="btn btn-primary">
-							<span class="icon-text">
-								<span class="icon">
-									<i class="fa-regular text-white fa-paper-plane"></i>
-								</span>
-								<span class="text-white">Tambah</span>
-							</span>
-						</button>
 					</div>
 				</div>
-			</div>
+				<div class="row">
+					<div class="col-6">
 
 
-	</form>
+						<div class="field">
+							<label class="mt-2">Email:</label>
+							<div class="control">
+								<input name="email" type="email" class="form-control">
+							</div>
+						</div>
+
+						<div class="field">
+							<label class="mt-2">Password:</label>
+							<div class="control">
+								<input name="password" type="password" class="form-control">
+							</div>
+						</div>
+
+						<div class="columns">
+							<div class="column is-half">
+								<div class="field">
+									<label class="mt-2">Jenis Kelamin:</label>
+									<div class="control">
+										<div class="select is-fullwidth">
+											<select name="kelamin" class="form-select">
+												<option value="pria"> Pria </option>
+												<option value="wanita"> Wanita </option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+					<div class="col-6">
+						<div class="column is-half">
+							<div class="field">
+								<label class="mt-2">Hak Akses:</label>
+								<div class="control">
+									<div class="select is-fullwidth">
+										<select name="level" class="form-select">
+											<option value="operator"> Operator </option>
+											<option value="viewer"> Viewer </option>
+											<option value="admin"> Admin </option>
+										</select>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="columns">
+							<div class="column is-half">
+								<div class="field">
+									<label class="mt-2">Bagian Kontrol:</label>
+									<div class="control">
+										<div class="select is-fullwidth">
+											<select name="segmen" class="form-select">
+												<option value="imb"> IMB </option>
+												<option value="krk"> KRK </option>
+												<option value="pengawasan"> Pengawasan </option>
+												<option value="semua"> Semua Bagian </option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="column is-half">
+								<div class="field">
+									<label class="mt-2">Status:</label>
+									<div class="control">
+										<div class="select is-fullwidth">
+											<select name="status" class="form-select">
+												<option value="aktif"> Aktif </option>
+												<option value="tidak aktif"> Tidak Aktif </option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+
+
+					</div>
+					<div class="row">
+						<div class="col">
+							<div class="field">
+								<label class="mt-2">Foto</label>
+								<div class="control">
+									<div class="file">
+										<label class="file-label d-flex align-items-center">
+											<input class="form-control" type="file" name="resume" />
+											<span class="file-cta ps-2" style="width: 250px;">
+												<span class="file-icon">
+													<i class="fas fa-upload"></i>
+												</span>
+												<span class="file-label"> Pilih file foto… </span>
+											</span>
+										</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<div class="field has-text-right mt-3 text-end">
+								<button name="tambah" class="btn btn-primary">
+									<span class="icon-text">
+										<span class="icon">
+											<i class="fa-regular text-white fa-paper-plane"></i>
+										</span>
+										<span class="text-white">Tambah</span>
+									</span>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+		</form>
+	</div>
 </div>
 <!-- Active Tab -->
 <?php
@@ -161,7 +190,7 @@ if ($_GET['p'] == 'user-add') {
 			tab.classList.remove('t-active', 'd-side-active');
 		});
 		// Tentukan tab active
-		const activeTab = document.querySelectorAll('.tab')[4];
+		const activeTab = document.querySelectorAll('.tab')[5];
 		// const activeDropdown = document.querySelectorAll('.tab-dropdown')[1];
 		// Tambahkan class aktif pada tab 
 		activeTab.classList.add('d-side-active', 't-active');
--- segmen/semua/table_users.php
+++ segmen/semua/table_users.php
@@ -1,67 +1,92 @@
-<div class="box">
-
-	<h3 class="title is-5">
-		Data Pengguna
-	</h3>
-	<hr>
-	<a href="?p=user-add" class="btn btn-primary mb-2">Tambah</a>
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>Nama Lengkap</th>
-				<th>Email</th>
-				<th>Jenis Kelamin</th>
-				<th>Hak Akses</th>
-				<th>Bagian</th>
-				<th>Status</th>
-				<th>Aksi</th>
-			</tr>
-		</thead>
-
-		<tbody>
-			<?php
-			$no = 1;
-			foreach ($cmd->fetchAll("SELECT * FROM akun") as $user):
-			?>
-				<tr>
-					<td><?= $no++; ?></td>
-					<td><?= $user['akun_nama']; ?></td>
-					<td><?= $user['akun_email']; ?></td>
-					<td><?= $user['akun_kelamin']; ?></td>
-					<td><?= $user['akun_level']; ?></td>
-					<td><?= $user['akun_segmen']; ?></td>
-					<td><?= $user['akun_status']; ?></td>
-					<td>
-						<div class="container d-flex gap-1">
-							<form action="" method="post" class="is-inline">
-								<input type="hidden" name="id" value="<?= $user['akun_id']; ?>">
-								<button class="btn btn-warning ">
-									<i class="fa-solid fa-pen-to-square text-white"></i>
-								</button>
-							</form>
-							<form action="" method="post" class="is-inline">
-								<input type="hidden" name="id" value="<?= $user['akun_id']; ?>">
-								<button class="btn btn-danger">
-									<i class="fa-solid fa-trash text-white"></i>
-								</button>
-							</form>
-						</div>
-					</td>
-				</tr>
-			<?php endforeach;
-			if ($no == 1) echo "<tr><td colspan=\"8\">Tidak ada data</td></tr>";
-			?>
-		</tbody>
-	</table>
-
-	<?php
-	$cheat = new Cheat();
-	$cheat->pages();
-	?>
+<div class="row">
+	<div class="col-12 p-0">
+		<div class="breadcrumbs">
+			<ul class="breadcrumb-nav ps-3">
+				<li><a href="">Dashboard</a></li>
+				<li><a href="">Pengguna</a></li>
+				<li class="text-primary">Data Pengguna</li>
+			</ul>
+		</div>
+	</div>
 </div>
+<!-- End Breadcrumbs -->
+<div class="box">
+	<div class="d-flex justify-content-between">
+		<h3 class="title is-5">
+			Data Pengguna
+		</h3>
+		<a href="?p=user-add" class="btn btn-primary">
+			<span class="icon-text">
+				<span class="icon">
+					<i class="fa-solid fa-plus text-white"></i>
+				</span>
+				<span class="text-white">Tambah</span>
+			</span>
+		</a>
+	</div>
+	<hr>
+	<div class="container card border-0 shadow rounded-4 p-3">
+		<div class="table-responsive">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nama Lengkap</th>
+						<th>Email</th>
+						<th>Jenis Kelamin</th>
+						<th>Hak Akses</th>
+						<th>Bagian</th>
+						<th>Status</th>
+						<th>Aksi</th>
+					</tr>
+				</thead>
 
-
+				<tbody>
+					<?php
+					$pages = $cheat->pages("SELECT * FROM akun", "user-data");
+					foreach ($pages['data'] as $user):
+					?>
+						<tr>
+							<td><?= $pages['no']++; ?></td>
+							<td><?= $user['akun_nama']; ?></td>
+							<td><?= $user['akun_email']; ?></td>
+							<td><?= $user['akun_kelamin']; ?></td>
+							<td><?= $user['akun_level']; ?></td>
+							<td><?= $user['akun_segmen']; ?></td>
+							<td><?= $user['akun_status']; ?></td>
+							<td>
+								<div class="d-flex gap-2">
+									<form action="" method="post" class="is-inline">
+										<input type="hidden" name="id" value="<?= $user['akun_id']; ?>">
+										<button class="btn btn-sm btn-warning">
+											<i class="fa-solid text-white fa-pen-to-square"></i>
+										</button>
+									</form>
+									<form action="?p=user-delete" method="post" class="is-inline">
+										<input type="hidden" name="id" value="<?= $user['akun_id']; ?>">
+										<button class="btn btn-sm btn-danger">
+											<i class="fa-solid text-white fa-trash"></i>
+										</button>
+									</form>
+								</div>
+							</td>
+						</tr>
+					<?php endforeach;
+					if ($pages['count'] == 0) echo "<tr><td colspan=\"8\">Tidak ada data</td></tr>";
+					?>
+				</tbody>
+			</table>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col d-flex justify-content-end flex-column align-items-end mt-2">
+					<?= $pages['links']; ?>
+					<p><?= $pages['detail']; ?></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <!-- Active Tab -->
 <?php
 if ($_GET['p'] == 'user-data') {
@@ -70,17 +95,20 @@ if ($_GET['p'] == 'user-data') {
 		document.querySelectorAll('.tab').forEach(tab => {
 			tab.classList.remove('t-active', 'd-side-active');
 		});
-		// Tentukan tab active
-		const activeTab = document.querySelectorAll('.tab')[5];
-		// const activeDropdown = document.querySelectorAll('.tab-dropdown')[1];
-		// Tambahkan class aktif pada tab 
+
+		// Tentukan tab dan dropdown yang ingin diaktifkan
+		const activeTab = document.querySelectorAll('.tab')[5]; // Misal, tab ke-3
+		// const activeDropdown = document.querySelectorAll('.tab-dropdown')[3]; // Misal, dropdown ke-2
+
+		// Tambahkan class aktif pada tab yang diinginkan
 		activeTab.classList.add('d-side-active', 't-active');
+
 		// Buat animasi transisi pada dropdown
-		// activeDropdown.style.height = activeDropdown.scrollHeight + "px";
-		// activeDropdown.addEventListener('transitionend', function handleTransitionEnd() {
-		// 	activeDropdown.style.height = 'auto';
-		// 	activeDropdown.removeEventListener('transitionend', handleTransitionEnd);
-		// });
+		activeDropdown.style.height = activeDropdown.scrollHeight + "px";
+		activeDropdown.addEventListener('transitionend', function handleTransitionEnd() {
+			activeDropdown.style.height = 'auto';
+			activeDropdown.removeEventListener('transitionend', handleTransitionEnd);
+		});
 	</script>
 <?php
 }
